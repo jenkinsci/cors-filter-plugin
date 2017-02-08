72155567e77cc8b6d144d6f9bf8c1d1eb1f75bad
Md. Ashiquzzaman <ashiquzzaman33@gmail.com>
2017-02-08 15:54:56 +0600

    Pathinfo check using regexp

--- src/main/java/org/jenkinsci/plugins/corsfilter/AccessControlsFilter.java
+++ src/main/java/org/jenkinsci/plugins/corsfilter/AccessControlsFilter.java
@@ -7,17 +7,21 @@ import hudson.init.Initializer;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.util.PluginServletFilter;
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Filter to support <a href="http://en.wikipedia.org/wiki/Cross-origin_resource_sharing">CORS</a>
@@ -29,136 +33,123 @@ import java.util.logging.Logger;
 @Extension
 public class AccessControlsFilter implements Filter, Describable<AccessControlsFilter> {
 
-    @Extension
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
-    private static final Logger LOGGER = Logger.getLogger(AccessControlsFilter.class.getCanonicalName());
+  @Extension
+  public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+  private static final Logger LOGGER = Logger
+      .getLogger(AccessControlsFilter.class.getCanonicalName());
+  private Pattern pattern = Pattern.compile("hello");
 
-    @Initializer(after = InitMilestone.JOB_LOADED)
-    public static void init() throws ServletException {
-        Injector inj = Jenkins.getInstance().getInjector();
-        if (inj == null) {
-            return;
+  @Initializer(after = InitMilestone.JOB_LOADED)
+  public static void init() throws ServletException {
+    Injector inj = Jenkins.getInstance().getInjector();
+    if (inj == null) {
+      return;
+    }
+    PluginServletFilter.addFilter(inj.getInstance(AccessControlsFilter.class));
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+
+  }
+
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    if (response instanceof HttpServletResponse) {
+
+      final HttpServletResponse resp = (HttpServletResponse) response;
+      if (request instanceof HttpServletRequest && getDescriptor().isEnabled()) {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        if (getDescriptor().isBlocked(req.getPathInfo())) {
+
+          resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+          LOGGER.log(Level.INFO, "Request blocked, URL: " + req.getPathInfo());
+          return;
         }
-        PluginServletFilter.addFilter(inj.getInstance(AccessControlsFilter.class));
+      }
+    }
+    chain.doFilter(request, response);
+  }
+  
+  @Override
+  public void destroy() {
+
+  }
+
+  @Override
+  public DescriptorImpl getDescriptor() {
+    return DESCRIPTOR;
+  }
+
+  public static final class DescriptorImpl extends Descriptor<AccessControlsFilter> {
+
+    private Pattern pattern;
+    private String[] blackListPrefix = new String[]{
+        "/restart",
+        "/safeRestart",
+        // Disable https://wiki.jenkins-ci.org/display/JENKINS/Jenkins+Script+Console
+        "/script",
+        "/scriptText",
+        "/credential-store",
+        "/asynchPeople",
+        "/people",
+        "/whoAmI",
+        "/updateCenter",
+        "/pluginManager",
+        "/scriptApproval",
+        "/log",
+        "/credentials",
+        "/cli",
+        "/about",
+        "/user",
+    };
+    private boolean enabled;
+
+    public DescriptorImpl() {
+      load();
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
+    public String getDisplayName() {
+      return "URL Filter";
     }
 
-    /**
-     * Handle CORS Access Controls
-     */
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        if (response instanceof HttpServletResponse) {
-
-            final HttpServletResponse resp = (HttpServletResponse) response;
-            if (request instanceof HttpServletRequest && getDescriptor().isEnabled()) {
-                HttpServletRequest req = (HttpServletRequest) request;
-                if (!isAllowed(req.getPathInfo())){
-                  resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-                  return;
-                }
-            }
-        }
-        chain.doFilter(request, response);
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+      setEnabled(json.getBoolean("enabled"));
+      setBlackList(json.getString("blackList"));
+      save();
+      return super.configure(req, json);
     }
 
-    /**
-     * Check if path is allowed
-     *
-     * @param pathInfo
-     * @return
-     */
-    public boolean isAllowed(String pathInfo) {
+    public boolean isBlocked(String pathInfo) {
       if (pathInfo == null) {
-        return true;
+        return false;
       }
-      String[] prefixBlackList = getDescriptor().getPrefixBlacklist();
-      for (int i=0; i<prefixBlackList.length; i++) {
-        if (pathInfo.startsWith(prefixBlackList[i])) {
-          return false;
-        }
-      }
-      return true;
+      return pattern.matcher(pathInfo).matches();
     }
 
-    @Override
-    public void destroy() {
-
+    public boolean isEnabled() {
+      return enabled;
     }
 
-    @Override
-    public DescriptorImpl getDescriptor() {
-        return DESCRIPTOR;
+    public void setEnabled(boolean enabled) {
+      this.enabled = enabled;
     }
 
-    public static final class DescriptorImpl extends Descriptor<AccessControlsFilter> {
-        private final String[] defaultBlackListPrefix = new String[] {
-            "/restart",
-            "/safeRestart",
-            // Disable https://wiki.jenkins-ci.org/display/JENKINS/Jenkins+Script+Console
-            "/script",
-            "/scriptText",
-            "/credential-store",
-            "/asynchPeople",
-            "/people",
-            "/whoAmI",
-            "/updateCenter",
-            "/pluginManager",
-            "/scriptApproval",
-            "/log",
-            "/credentials",
-            "/cli",
-            "/about",
-            "/user",
-        };
-        private String[] prefixBlacklist;
-        private boolean enabled;
-
-        public DescriptorImpl() {
-            load();
-        }
-
-        @Override
-        public String getDisplayName() {
-            return "URL Filter";
-        }
-
-        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
-            enabled = json.getBoolean("enabled");
-            String blackList = json.getString("blackList");
-            prefixBlacklist = blackList.split("\n");
-            save();
-            return super.configure(req, json);
-        }
-
-        public boolean isEnabled() {
-            return enabled;
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.enabled = enabled;
-        }
-
-        public String[] getPrefixBlacklist() {
-          if (prefixBlacklist == null) {
-            prefixBlacklist = Arrays.copyOf(defaultBlackListPrefix, defaultBlackListPrefix.length);
-          }
-          return prefixBlacklist;
-        }
-
-        public String getBlackList() {
-          return String.join("\n", getPrefixBlacklist());
-        }
-
-        public void setBlackList(String blackList) {
-          prefixBlacklist = blackList.split("\n");
-        }
-
-        public void setPrefixBlacklist(String[] prefixBlacklist) { this.prefixBlacklist = prefixBlacklist; }
-
+    public String getBlackList() {
+      return String.join("\n", blackListPrefix);
     }
+
+    public void setBlackList(String blackList) {
+      blackList = blackList.replaceAll("\\h", "");
+      blackListPrefix = blackList.split("\n");
+      compilePattern();
+    }
+
+    public void compilePattern() {
+      String regex = String.format("(%s)($|/.*)", String.join("|", blackListPrefix));
+      pattern = Pattern.compile(regex);
+    }
+  }
 }
